dfaafeefb7cc461e3db2c15d9fa670e77548b66f
Amit <fulldev101@gmail.com>
2020-11-30 22:08:15 +0530

    adding requirements.txt file\

---
+++ keylogger.py
@@ -0,0 +1,8 @@
+'''
+Hi this is a tutorial for understanding and development of keylogger in python.
+This is a very simple keylogger but we will try to improve it with time.
+'''
+
+import pynput
+
+print(pynput)
